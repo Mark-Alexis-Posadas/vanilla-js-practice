3e1ca854d39d8cea5cd09e7cd736aeb67c46c96e
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-04-28 11:43:54 +0800

    added markdown folder in loops folder

--- js/index.js
+++ js/index.js
@@ -8,3 +8,46 @@
 //   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 // ];
 // filterEvenNumbers(data);
+
+//Filter out odd numbers from an array.
+
+// const filterOddNumbers = (numbers) => {
+//   const result = numbers.filter((number) => number % 2);
+//   console.log(result);
+// };
+
+// let data = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+// ];
+// filterOddNumbers(data);
+
+//Filter out strings longer than a certain length from an array of strings.
+
+//Filter out numbers greater than a specific value from an array of numbers.
+// const filterHighestNumbers = (numbers) => {
+//   const result = numbers.filter((number) => number > 4);
+//   console.log(result);
+// };
+
+// filterHighestNumbers([1, 4, 51, 28, 93, 78, 21, 34, 56, 53, 78, 90]);
+
+//Filter out negative numbers from an array of mixed numbers.
+
+// const filterNegativeNumbers = (numbers) => {
+//   const result = numbers.filter((number) => number < 0);
+//   console.log(result);
+// };
+
+// filterNegativeNumbers([-1, 4, 51, -28, 93, -78, 21, -34, 56, 53, 78, -90]);
+
+//Filter out positive numbers from an array of mixed numbers.
+// const filterPositiveNumbers = (numbers) => {
+//   const result = numbers.filter((number) => number >= 0);
+//   console.log(result);
+// };
+
+// filterPositiveNumbers([-1, 4, 51, -28, 93, -78, 21, -34, 56, 0, 53, 78, -90]);
+
+//Filter out words that start with a specific letter from an array of strings.
+
+//Filter out objects with a certain property value from an array of objects.
