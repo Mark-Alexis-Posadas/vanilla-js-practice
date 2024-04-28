51d9982230903143e8b1a4431d6290c79598c395
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-04-28 08:37:14 +0800

    added filter even numbers code in filter

---
+++ js/loops/filter.js
@@ -0,0 +1,11 @@
+//Filter out even numbers from an array.
+const filterEvenNumbers = (numbers) => {
+  console.log(numbers);
+  // const result = numbers.filter((number) => number % 2);
+  // console.log(result);
+};
+
+let data = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+filterEvenNumbers(data);
