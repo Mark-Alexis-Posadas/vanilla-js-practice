9f93f13b06f84bb97e21602ab8ac6367c46681e2
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-02 10:44:03 +0800

    added checkday exercise

---
+++ index.js
@@ -0,0 +1,22 @@
+//A program that takes a number and prints out the corresponding day of the week.
+function checkDay(day) {
+  switch (day) {
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    case 7:
+      return "Sunday";
+    default:
+      return "Not found";
+  }
+}
+console.log(checkDay(1));
