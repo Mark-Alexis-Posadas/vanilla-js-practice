895e698076a276940b5532f7647f7fa4e7f0a892
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-03 13:31:43 +0800

    additional exercises in if else

--- Problems.md
+++ Problems.md
@@ -1,23 +1,3 @@
-    Exercise 1: Even or Odd
-        Input: A number
-        Output: "Even" if the number is even, "Odd" if the number is odd.
-
-    Exercise 2: Grade Evaluation
-        Input: A numerical grade (0-100)
-        Output: "Pass" if the grade is 60 or above, "Fail" if below 60.
-
-    Exercise 3: Age Group Determination
-        Input: Age
-        Output: "Child" if age is less than 13, "Teenager" if age is between 13 and 19, "Adult" if age is 20 or above.
-
-    Exercise 4: Temperature Check
-        Input: Temperature in Celsius
-        Output: "Hot" if temperature is above 30°C, "Moderate" if between 20°C and 30°C, "Cold" if below 20°C.
-
-    Exercise 5: Username Validation
-        Input: Username string
-        Output: "Valid" if the username contains at least 6 characters and doesn't contain any special characters, "Invalid" otherwise.
-
     Exercise 6: Leap Year Check
         Input: Year
         Output: "Leap year" if the year is divisible by 4 and not divisible by 100 unless it is also divisible by 400, "Not a leap year" otherwise.
@@ -29,11 +9,3 @@
     Exercise 8: Positive, Negative, or Zero
         Input: A number
         Output: "Positive" if the number is greater than 0, "Negative" if less than 0, "Zero" if equal to 0.
-
-    Exercise 9: Triangle Type
-        Input: Lengths of three sides of a triangle
-        Output: "Equilateral" if all sides are equal, "Isosceles" if any two sides are equal, "Scalene" if all sides are different.
-
-    Exercise 10: Discount Calculation
-        Input: Purchase amount
-        Output: Final amount to be paid after applying a 10% discount if the purchase amount is above $1000, otherwise, the output will be the same as the input amount.
--- index.js
+++ index.js
@@ -1,22 +1,41 @@
-//Grade Evaluation
-// function gradeEvaluation(grade) {
-//   if (grade < 60) {
-//     return "Fail";
-//   }
-//   return "Pass";
-// }
-// console.log(gradeEvaluation(61)); //Pass
-// console.log(gradeEvaluation(59)); //Fail
+//Exercise 5: Username Validation
+//Input: Username string
+//Output: "Valid" if the username contains at least 6 characters and doesn't contain any special characters, "Invalid" otherwise.
 
-//Age Group Determination
-// function ageGroupDetermination(age) {
-//   if (age < 13) {
-//     return "Child";
-//   } else if (age > 19) {
-//     return "Adult";
-//   } else {
-//     return "Teenager";
+// function usernameValidation(name, specialCharacter) {
+//   if (name.length <= 6 && name === specialCharacter) {
+//     return "Valid";
 //   }
+//   return "Invalid";
+// }
+// const specialChar = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+// console.log(usernameValidation("jakejc", specialChar));
+
+// Exercise 10: Discount Calculation
+// Input: Purchase amount
+// Output: Final amount to be paid after applying a 10% discount if the purchase amount is above $1000, otherwise, the output will be the same as the input amount.
+
+// function discountCalculation(price) {
+//   if (price >= 1000) {
+//     return price - price * 0.1;
+//   }
+//   return price;
 // }
 
-// console.log(ageGroupDetermination(12));
+// console.log(discountCalculation(1100));
+
+// Exercise 8: Positive, Negative, or Zero
+// Input: A number
+// Output: "Positive" if the number is greater than 0, "Negative" if less than 0, "Zero" if equal to 0.
+
+function checkNumber(number) {
+  if (number > 0) {
+    return "Positive";
+  } else if (number < 0) {
+    return "Negative";
+  } else {
+    return "Zero";
+  }
+}
+
+console.log(checkNumber(20));
