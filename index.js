2b02ea23469f2b2424a1a5ef648fd726486ed391
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-06 20:33:57 +0800

    additional exercises in objects

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-// Create an object representing a list of students with their grades in different subjects. Use a loop to print each student's name along with their average grade.
+// Create an object representing a list of products with their prices. Use a loop to print each product's name along with whether its price is above or below a certain threshold (e.g., $50).
 
-let employees = [
-  { name: "Emily", salary: 40000 },
-  { name: "David", salary: 60000 },
-  { name: "Sophia", salary: 80000 },
+function productPrices(products) {
+  let results = [];
+  for (let i = 0; i < products.length; i++) {
+    results.push(
+      `Product: ${products[i].name}, price ${
+        products[i].price >= 800 ? "Above Threshold" : "Below Threshold"
+      }`
+    );
+  }
+
+  return results;
+}
+
+let products = [
+  { name: "Laptop", price: 1200 },
+  { name: "Smartphone", price: 800 },
+  { name: "Headphones", price: 60 },
 ];
 
-for (let i = 0; i < employees.length; i++) {
-  let result = `Employee: ${employees[i].name}, Salary: ${
-    employees[i].salary <= 40000
-      ? "Low"
-      : employees[i].salary > 40000 && employees[i].salary <= 60000
-      ? "Medium"
-      : "High"
-  }`;
+let result = productPrices(products);
 
-  console.log(result);
-}
+console.log(result);
--- js/objects/objects.js
+++ js/objects/objects.js
@@ -57,3 +57,28 @@ for (let i = 0; i < employees.length; i++) {
 
   console.log(result);
 }
+
+// Create an object representing a list of products with their prices. Use a loop to print each product's name along with whether its price is above or below a certain threshold (e.g., $50).
+
+function productPrices(products) {
+  let results = [];
+  for (let i = 0; i < products.length; i++) {
+    results.push(
+      `Product: ${products[i].name}, price ${
+        products[i].price >= 800 ? "Above Threshold" : "Below Threshold"
+      }`
+    );
+  }
+
+  return results;
+}
+
+let products = [
+  { name: "Laptop", price: 1200 },
+  { name: "Smartphone", price: 800 },
+  { name: "Headphones", price: 60 },
+];
+
+let result = productPrices(products);
+
+console.log(result);
