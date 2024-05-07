a6d58ef61069e4e4a6320d74ca435001d61d999c
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-07 11:38:45 +0800

    added song duration in objects exercise

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-// Create an object representing a list of products with their prices. Use a loop to print each product's name along with whether its price is above or below a certain threshold (e.g., $50).
+// Create an object representing a list of songs with their durations in seconds. Use a loop to print each song's name along with its duration in minutes and seconds.
 
-function productPrices(products) {
+function songDuration(songs) {
   let results = [];
-  for (let i = 0; i < products.length; i++) {
-    results.push(
-      `Product: ${products[i].name}, price ${
-        products[i].price >= 800 ? "Above Threshold" : "Below Threshold"
-      }`
-    );
+
+  for (let i = 0; i < songs.length; i++) {
+    const element = `Song ${songs[i].name}, ${songs[i].duration / 60}`;
+    results.push(element);
   }
 
   return results;
 }
-
-let products = [
-  { name: "Laptop", price: 1200 },
-  { name: "Smartphone", price: 800 },
-  { name: "Headphones", price: 60 },
+let songs = [
+  { name: "Bohemian Rhapsody", duration: 367 },
+  { name: "Hotel California", duration: 391 },
+  { name: "Stairway to Heaven", duration: 482 },
 ];
 
-let result = productPrices(products);
+let result = songDuration(songs);
 
 console.log(result);
