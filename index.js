76c5599a3ba6b36b275bb5aec25571a765ef3aab
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-08 19:57:48 +0800

    fix filter functio naming

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-// Leap Year Checker:
-// Write a function that takes a year as a parameter and returns true if it's a leap year, otherwise false. Leap years are divisible by 4, but not by 100 unless they are also divisible by 400.
-
 // Absolute Value:
 // Write a function that takes a number as a parameter and returns its absolute value.
--- js/loops/filter.js
+++ js/loops/filter.js
@@ -1,89 +1,80 @@
 //Filter out even numbers from an array.
-// const filterEvenNumbers = (numbers) => {
-//   return numbers.filter((number) => number % 2 == 0);
-// };
+const filterEvenNumbers = (numbers) => {
+  return numbers.filter((number) => number % 2 == 0);
+};
 
-// let data = [
-//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-// ];
-// const result = filterEvenNumbers(data);
-// console.log(result); //2,4,6,8,10,12,14,16,18,20
+let numbersEvenData = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+
+console.log(filterEvenNumbers(numbersEvenData)); //2,4,6,8,10,12,14,16,18,20
 
 //Filter out odd numbers from an array.
-// const filterOddNumbers = (numbers) => {
-//   return numbers.filter((number) => number % 2);
-// };
+const filterOddNumbers = (numbers) => {
+  return numbers.filter((number) => number % 2);
+};
 
-// let data = [
-//   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-// ];
-// const result = filterOddNumbers(data);
-// console.log(result);
+let numbersOddData = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+const result = console.log(filterOddNumbers(numbersOddData));
 
 //Filter out numbers greater than a specific value from an array of numbers.
-// const filterNumbersGreaterThan = (numbers, threshold) => {
-//   return numbers.filter((number) => number > threshold);
-// };
+const filterNumbersGreaterThan = (numbers, threshold) => {
+  return numbers.filter((number) => number > threshold);
+};
 
-// let data = [1, 4, 51, 28, 93, 78, 21, 34, 56, 53, 78, 90];
-// let thresholdValue = 3;
+let greaterThanNumberData = [1, 4, 51, 28, 93, 78, 21, 34, 56, 53, 78, 90];
+let thresholdValue = 3;
 
-// const result = filterNumbersGreaterThan(data, thresholdValue);
-// console.log(result);
+console.log(filterNumbersGreaterThan(greaterThanNumberData, thresholdValue));
 
 //Filter out negative numbers from an array of mixed numbers.
-// const filterNegativeNumbers = (numbers) => {
-//   return numbers.filter((number) => number < 0);
-// };
-// let data = [-1, 4, 51, -28, 93, -78, 21, -34, 56, 53, 78, -90];
-// let result = filterNegativeNumbers(data);
-// console.log(result);
+const filterNegativeNumbers = (numbers) => {
+  return numbers.filter((number) => number < 0);
+};
+let data = [-1, 4, 51, -28, 93, -78, 21, -34, 56, 53, 78, -90];
+console.log(filterNegativeNumbers(data));
 
 //Filter out positive numbers from an array of mixed numbers.
-// const filterPositiveNumbers = (numbers) => {
-//   const result = numbers.filter((number) => number >= 0);
-//   return result;
-// };
+const filterPositiveNumbers = (numbers) => {
+  const result = numbers.filter((number) => number >= 0);
+  return result;
+};
 
-// const output = filterPositiveNumbers([
-//   -1, 4, 51, -28, 93, -78, 21, -34, 56, 0, 53, 78, -90,
-// ]);
-
-// console.log(output);
+console.log(
+  filterPositiveNumbers([-1, 4, 51, -28, 93, -78, 21, -34, 56, 0, 53, 78, -90])
+);
 
 //Filter out names with if names contains letter a or d
-// function filterNames(names) {
-//   const result = names.filter(
-//     (name) => name.includes("a") || name.includes("d")
-//   );
-//   return result;
-// }
+function filterNames(names) {
+  const result = names.filter(
+    (name) => name.includes("a") || name.includes("d")
+  );
+  return result;
+}
 
-// const output = filterNames(["mark", "jojo", "jed", "alex", "jean", "roxas"]);
-// console.log(output);
+console.log(filterNames(["mark", "jojo", "jed", "alex", "jean", "roxas"]));
 
 //Filter out objects with a certain property value from an array of objects.
 
-// function getOlderPersons(persons) {
-//   const result = persons.filter((person) => person.age >= 25);
-//   return result;
-// }
+function getOlderPersons(persons) {
+  const result = persons.filter((person) => person.age >= 25);
+  return result;
+}
 
-// const persons = [
-//   { name: "Juan", age: 25 },
-//   { name: "Maria", age: 17 },
-//   { name: "Pedro", age: 30 },
-//   { name: "Luis", age: 16 },
-// ];
+const persons = [
+  { name: "Juan", age: 25 },
+  { name: "Maria", age: 17 },
+  { name: "Pedro", age: 30 },
+  { name: "Luis", age: 16 },
+];
 
-// let output = getOlderPersons(persons);
-// console.log(output);
+console.log(getOlderPersons(persons));
 //Filter out strings longer than a certain length from an array of strings.
+function filterLongestString(names) {
+  const result = names.filter((name) => name.length >= 5);
+  return result;
+}
 
-// function filterLongestString(names) {
-//   const result = names.filter((name) => name.length >= 5);
-//   return result;
-// }
-
-// const output = filterLongestString(["jake", "juan", "jane", "rodolfo"]);
-// console.log(output); //rodolfo
+console.log(filterLongestString(["jake", "juan", "jane", "rodolfo"])); //rodolfo
