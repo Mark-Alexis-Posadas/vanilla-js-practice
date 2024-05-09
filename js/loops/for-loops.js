1db091049ef879ac3824903a0a88133134899d0c
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-09 19:26:27 +0800

    uncomment codes in for loops

--- js/loops/for-loops.js
+++ js/loops/for-loops.js
@@ -1,110 +1,104 @@
 //Write a program to print numbers from 1 to 10 using a for loop.
-// for (let i = 1; i <= 10; i++) {
-//   console.log(i);
-// }
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
 
 //Create a loop that prints even numbers from 0 to 20.
 
-// for (let i = 0; i <= 20; i++) {
-//   console.log(i);
-// }
+for (let i = 0; i <= 20; i++) {
+  console.log(i);
+}
 
 //Write a loop that counts down from 10 to 1.
-// for (let i = 10; i >= 1; i--) {
-//   console.log(i);
-// }
+for (let i = 10; i >= 1; i--) {
+  console.log(i);
+}
 
 //Create a loop that iterates over an array of names and prints each name to the console.
 
-// const arrayOfNames = ["mark", "alexis", "juan"];
-// for (let i = 0; i < arrayOfNames.length; i++) {
-//   console.log(arrayOfNames[i]);
-// }
+const arrayOfNames = ["mark", "alexis", "juan"];
+for (let i = 0; i < arrayOfNames.length; i++) {
+  console.log(arrayOfNames[i]);
+}
 
 //Multiplication table DISPLAY
-// const multiplicationTable = (numbers) => {
-//   let addSence = 1;
-//   for (let i = 1; i <= numbers.length; i++) {
-//     console.log(
-//       `${addSence} * ${i}  = ${i}`,
-//       `${addSence + 1} * ${i}  = ${i * 2}`,
-//       `${addSence + 2} * ${i}  = ${i * 3}`,
-//       `${addSence + 3} * ${i}  = ${i * 4}`,
-//       `${addSence + 4} * ${i}  = ${i * 5}`,
-//       `${addSence + 5} * ${i}  = ${i * 6}`,
-//       `${addSence + 6} * ${i}  = ${i * 7}`,
-//       `${addSence + 7} * ${i}  = ${i * 8}`,
-//       `${addSence + 8} * ${i}  = ${i * 9}`,
-//       `${addSence + 9} * ${i}  = ${i * 10}`
-//     );
-//   }
-// };
+const multiplicationTable = (numbers) => {
+  let addSence = 1;
+  for (let i = 1; i <= numbers.length; i++) {
+    console.log(
+      `${addSence} * ${i}  = ${i}`,
+      `${addSence + 1} * ${i}  = ${i * 2}`,
+      `${addSence + 2} * ${i}  = ${i * 3}`,
+      `${addSence + 3} * ${i}  = ${i * 4}`,
+      `${addSence + 4} * ${i}  = ${i * 5}`,
+      `${addSence + 5} * ${i}  = ${i * 6}`,
+      `${addSence + 6} * ${i}  = ${i * 7}`,
+      `${addSence + 7} * ${i}  = ${i * 8}`,
+      `${addSence + 8} * ${i}  = ${i * 9}`,
+      `${addSence + 9} * ${i}  = ${i * 10}`
+    );
+  }
+};
 
-// multiplicationTable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+multiplicationTable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
 //optimize way
 
-// const multiplicationTable = (numbers) => {
-//   for (let i = 1; i <= 10; i++) {
-//     let row = "";
-//     for (let j = 0; j < numbers.length; j++) {
-//       const product = numbers[j] * i;
-//       row += `${numbers[j]} * ${i} = ${product} `;
-//     }
-//     console.log(row);
-//   }
-// };
+const multiplicationTableOptimizeWay = (numbers) => {
+  for (let i = 1; i <= 10; i++) {
+    let row = "";
+    for (let j = 0; j < numbers.length; j++) {
+      const product = numbers[j] * i;
+      row += `${numbers[j]} * ${i} = ${product} `;
+    }
+    console.log(row);
+  }
+};
 
-// multiplicationTable([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-
-//Create a loop that iterates over a string and prints each character separately.
-// function printEachCharacter(name) {
-//   for (let i = 0; i < name.length; i++) console.log(name[i]);
-// }
-// printEachCharacter("MarkAlexisPosadas");
+multiplicationTableOptimizeWay([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 
 // Find the largest number in an array.
-// function findLargestNumber(numbers) {
-//   let result;
-//   for (let i = 0; i < numbers.length; i++) {
-//     result = numbers[i];
-//   }
-//   return result;
-// }
-// const output = findLargestNumber([1, 2, 3]);
-// console.log(output);
+function findLargestNumber(numbers) {
+  let result;
+  for (let i = 0; i < numbers.length; i++) {
+    result = numbers[i];
+  }
+  return result;
+}
 
-// function findLargestNumber(numbers) {
-//   let result;
-//   for (let i = 0; i < numbers.length; i++) {
-//     result = numbers[2];
-//   }
-//   return result;
-// }
-// const output = findLargestNumber([1, 2, 3]);
-// console.log(output);
+console.log(findLargestNumber([1, 2, 3]));
 
-// function findLargestNumber(numbers) {
-//   let result = numbers[0];
-//   for (let i = 1; i < numbers.length; i++) {
-//     if (numbers[i] > result) {
-//       result = numbers[i];
-//     }
-//   }
-//   return result;
-// }
-// const output = findLargestNumber([1, 2, 3]);
-// console.log(output);
+function findLargestNumber(numbers) {
+  let result;
+  for (let i = 0; i < numbers.length; i++) {
+    result = numbers[2];
+  }
+  return result;
+}
+
+console.log(findLargestNumber([1, 2, 3]));
+
+function findLargestNumber(numbers) {
+  let result = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > result) {
+      result = numbers[i];
+    }
+  }
+  return result;
+}
+
+console.log(findLargestNumber([1, 2, 3]));
 
 //Remove duplicate elements from an array.
-// function removeDuplicateArray(array) {
-//   let unique = [];
-//   for (let index = 0; index < array.length; index++) {
-//     if (!unique.includes(array[index])) {
-//       unique.push(array[index]);
-//     }
-//   }
-//   return unique;
-// }
+function removeDuplicateArray(array) {
+  let unique = [];
+  for (let index = 0; index < array.length; index++) {
+    if (!unique.includes(array[index])) {
+      unique.push(array[index]);
+    }
+  }
+  return unique;
+}
 
-// console.log(removeDuplicateArray([1, 2, 3, 4, 2, 3, 5, 6]));
+console.log(removeDuplicateArray([1, 2, 3, 4, 2, 3, 5, 6]));
