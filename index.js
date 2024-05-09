4a51616d37137c1a5657a0887132671f5ff8f58f
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-09 18:41:34 +0800

    added  check uniformity exercise

--- index.js
+++ index.js
@@ -2,21 +2,6 @@
 // Input: [1, 2, 3, 4, 5]
 // Output: [1, 4, 9, 16, 25]
 
-// Checking for Existence of an Element:
-// Input: [10, 20, 30, 40, 50], 40
-const checkExistenceElement = (paramOne, paramTwo) => {
-  for (let i = 0; i < paramOne.length; i++) {
-    if (paramOne[i] === paramTwo) {
-      return true;
-    }
-  }
-  return false;
-};
-
-console.log(checkExistenceElement([10, 20, 30, 40, 50], 20));
-
-// Output: true
-
 // Combining Arrays:
 // Input: [1, 2, 3], [4, 5, 6]
 // Output: [1, 2, 3, 4, 5, 6]
@@ -27,4 +12,20 @@ console.log(checkExistenceElement([10, 20, 30, 40, 50], 20));
 
 // Checking for Uniformity in an Array:
 // Input: [1, 1, 1, 1, 1]
+
+const checkUniformity = (param) => {
+  let first = param[0];
+  let ans = true;
+
+  param.forEach((item) => {
+    if (first !== item) {
+      ans = false;
+    }
+  });
+
+  return ans;
+};
+
+console.log(checkUniformity([20, 20, 20]));
+
 // Output: true
