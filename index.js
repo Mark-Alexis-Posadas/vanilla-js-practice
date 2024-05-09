d0ff7bd362d9f476262e3d26331c2e407460b5ca
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-09 19:11:15 +0800

    done array methods exercises

--- index.js
+++ index.js
@@ -1,31 +1,14 @@
-// Mapping Array of Numbers to their Squares:
-// Input: [1, 2, 3, 4, 5]
-// Output: [1, 4, 9, 16, 25]
-
-// Combining Arrays:
-// Input: [1, 2, 3], [4, 5, 6]
-// Output: [1, 2, 3, 4, 5, 6]
-
 // Flattening an Array of Arrays:
 // Input: [[1, 2], [3, 4], [5, 6]]
-// Output: [1, 2, 3, 4, 5, 6]
-
-// Checking for Uniformity in an Array:
-// Input: [1, 1, 1, 1, 1]
-
-const checkUniformity = (param) => {
-  let first = param[0];
-  let ans = true;
-
-  param.forEach((item) => {
-    if (first !== item) {
-      ans = false;
-    }
-  });
-
-  return ans;
+const FlatteningAnArrayOfArrays = (arr) => {
+  return arr.flat(1);
 };
+console.log(
+  FlatteningAnArrayOfArrays([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ])
+);
 
-console.log(checkUniformity([20, 20, 20]));
-
-// Output: true
+// Output: [1, 2, 3, 4, 5, 6]
--- js/array/array-methods.js
+++ js/array/array-methods.js
@@ -21,12 +21,24 @@ const findMaxNumber = (numbers) => {
 };
 
 console.log(findMaxNumber([10, 20, 4, 15]));
+
+//
+
+// small version
+// const findMaxNumber = (numbers) => {
+//   let max = numbers[0];
+//   numbers.forEach((number) => (max < number ? (max = number) : max));
+//   return max;
+// };
+
+// console.log(findMaxNumber([10, 20, 4, 15]));
+
 // Output: 20
 
 // Counting Occurrences:
 // Input: ['apple', 'banana', 'apple', 'orange', 'apple']
 const countingOccurences = (param) => {
-  let counter = [];
+  let counter = {};
   param.forEach((item) => {
     if (counter[item]) {
       counter[item] += 1;
@@ -54,3 +66,57 @@ const checkExistenceElement = (paramOne, paramTwo) => {
 
 console.log(checkExistenceElement([10, 20, 30, 40, 50], 20));
 // Output: true
+
+// Checking for Uniformity in an Array:
+// Input: [1, 1, 1, 1, 1]
+
+const checkUniformity = (param) => {
+  let first = param[0];
+  let ans = true;
+
+  param.forEach((item) => {
+    if (first !== item) {
+      ans = false;
+    }
+  });
+
+  return ans;
+};
+
+console.log(checkUniformity([1, 1, 1]));
+// Output: true
+
+// Mapping Array of Numbers to their Squares:
+// Input: [1, 2, 3, 4, 5]
+const arrayofNumbersSquares = (nums) => {
+  return nums.map((num) => Math.pow(num, 2));
+};
+console.log(arrayofNumbersSquares([1, 2, 3, 4, 5]));
+
+// Output: [1, 4, 9, 16, 25]
+
+// Combining Arrays:
+// Input: [1, 2, 3], [4, 5, 6]
+
+const combiningArrays = (arrOne, arrTwo) => {
+  return [...arrOne, ...arrTwo];
+};
+
+console.log(combiningArrays([1, 2, 3], [4, 5, 6]));
+
+// Output: [1, 2, 3, 4, 5, 6]
+
+// Flattening an Array of Arrays:
+// Input: [[1, 2], [3, 4], [5, 6]]
+const FlatteningAnArrayOfArrays = (arr) => {
+  return arr.flat(1);
+};
+console.log(
+  FlatteningAnArrayOfArrays([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ])
+);
+
+// Output: [1, 2, 3, 4, 5, 6]
