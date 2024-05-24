0698ee5d902dd71bca98650ab0964cf9812188b4
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-24 12:27:43 +0800

    added number in range exercises

--- index.js
+++ index.js
@@ -1,49 +1,13 @@
-// Day of the Week
-// Input: A number from 1 to 7
-// Output: The corresponding day of the week (1 for Monday, 2 for Tuesday, etc.).
+// Number in Range
+// Input: A number
+// Output: "In range" if the number is between 1 and 100, "Out of range" otherwise.
 
-function dayOfTheWeek(day) {
-  if (day === 1) {
-    return "Monday";
-  } else if (day === 2) {
-    return "Tuesday";
-  } else if (day === 3) {
-    return "Wednesday";
-  } else if (day === 4) {
-    return "Thursday";
-  } else if (day === 5) {
-    return "Friday";
-  } else if (day === 6) {
-    return "Saturday";
-  } else if (day === 7) {
-    return "Sunday";
+function numberInRage(number) {
+  if (number >= 1 && number <= 100) {
+    return "In range";
   } else {
-    return "Holiday";
+    return "Out of range";
   }
 }
 
-console.log(dayOfTheWeek(5));
-
-function dayOfTheWeek(day, name) {
-  let result = [];
-  for (let i = 0; i < day.length; i++) {
-    result.push(`${day[i]} ${name[i]}`);
-  }
-
-  return result;
-}
-
-console.log(
-  dayOfTheWeek(
-    [1, 2, 3, 4, 5, 6, 7],
-    [
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-      "Sunday",
-    ]
-  )
-);
+console.log(numberInRage(1));
--- js/conditional-statement/if-else.js
+++ js/conditional-statement/if-else.js
@@ -199,3 +199,17 @@ console.log(
     ]
   )
 );
+
+// Number in Range
+// Input: A number
+// Output: "In range" if the number is between 1 and 100, "Out of range" otherwise.
+
+function numberInRage(number) {
+  if (number >= 1 && number <= 100) {
+    return "In range";
+  } else {
+    return "Out of range";
+  }
+}
+
+console.log(numberInRage(1));
