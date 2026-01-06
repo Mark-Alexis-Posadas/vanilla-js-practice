d72f27174854d6d174fce5f5b1ab328daae26ec2
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2026-01-06 11:23:48 +0800

    [feat] added challenge number 3

--- 2025-grind/january/day-one/challenge-3.js
+++ 2025-grind/january/day-one/challenge-3.js
@@ -1,16 +1,13 @@
 function mgaHayopSaPalayan(animals) {
   let waterAnimals = [];
   let landAnimals = [];
-
   for (let i = 0; i < animals.length; i++) {
-    if (
-      animals[i] === "tagak" ||
-      animals[i] === "tilapia" ||
-      animals[i] === "bangus"
-    ) {
-      waterAnimals.push(animals[i]);
+    if (animals[i].name === "Kalabaw") {
+      continue;
+    } else if (animals[i].habitat === "water") {
+      waterAnimals.push(animals[i].name);
     } else {
-      landAnimals.push(animals[i]);
+      landAnimals.push(animals[i].name);
     }
   }
   return { water: waterAnimals, land: landAnimals };
--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const mgaHayopSaPalayan = require("./2025-grind/january/day-one/challenge-3.js");
 
 // sample array na ipapasa sa function
-const animals = ["tagak", "kalabaw", "tilapia", "kambing", "bangus"];
+const animals = [
+  { name: "Tagak", habitat: "water" },
+  { name: "Kalabaw", habitat: "land" },
+  { name: "Kambing", habitat: "land" },
+  { name: "Tilapia", habitat: "water" },
+  { name: "Bangus", habitat: "water" },
+  { name: "Kingfisher", habitat: "water" },
+];
 
 console.log(mgaHayopSaPalayan(animals));
 
