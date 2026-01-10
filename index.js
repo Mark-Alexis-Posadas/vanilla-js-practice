ab8b45a17586fcd93004e38d964c8278218f57e1
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2026-01-10 20:14:38 +0800

    [feat] added challengen number 6

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const totalPricePerCategory = require("./2025-grind/january/day-three/challenge-5.js");
-const items = [
-  { name: "Bigas", category: "food", price: 120 },
-  { name: "Isda", category: "food", price: 80 },
-  { name: "Sabon", category: "hygiene", price: 45 },
+const finalBossChallenge = require("./2025-grind/january/day-three/challenge-6.js");
+const products = [
+  { name: "Rice", category: "food", price: 50 },
+  { name: "Chicken", category: "food", price: 180 },
+  { name: "Soap", category: "hygiene", price: 40 },
   { name: "Shampoo", category: "hygiene", price: 120 },
-  { name: "Notebook", category: "school", price: 60 },
+  { name: "Beer", category: "drinks", price: 90 },
 ];
 
-console.log(totalPricePerCategory(items));
+console.log(finalBossChallenge(products));
